b0ff57c49267f7e9e8f4bfbe8c1116275e7ae578
Victor Fernandes <victorr5r@gmail.com>
2026-04-10 21:30:36 -0300

    Refact: Novo template

--- device-monitor-ui/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { TerminalComponent } from './features/terminal/pages/terminal/terminal.component';
-
-
-export const routes: Routes = [
-
-  { path: "**" , component: TerminalComponent},
-];
